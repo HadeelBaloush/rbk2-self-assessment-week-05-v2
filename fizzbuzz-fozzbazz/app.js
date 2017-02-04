e488773decde47bc2e5b0e95ccbaa605ded9f19c
HadeelBaloush <hadeelbaloush@gmail.com>
2017-02-04 11:14:25 +0400

    1

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -18,6 +18,48 @@ angular.module('app', ['ngRoute'])
 /*  HINT: Make sure your controllers, methods, and variables 
     are named what $routeProvider and the partials are expecting  */
 
-  .factory('counter', function(){})
-  .controller('fizzbuzzCtrl', function(){})
-  .controller('fozzbazzCtrl', function(){});
+  .factory('counter', function(){
+    return 0;
+
+    // {
+    //   count: 0,
+    //   // addByOne: function(){
+    //   //   count++;
+    //   // }
+    // }
+  })
+  .controller('fizzbuzzCtrl', function($scope, counter){
+    $scope.display = '';
+    $scope.increment = function(){
+      counter++;
+      if(counter%3 === 0){
+        $scope.display = 'FIZZ';
+      }
+      else if(counter%5 === 0){
+        $scope.display = 'BUZZ';
+      }
+      else if(counter%5 === 0 && counter%3 === 0){
+        $scope.display = 'FIZZBUZZ';
+      }
+
+      // .addByOne().then(function(){
+      //   console.log('It is incremented by one');
+      // });
+    }
+  })
+  .controller('fozzbazzCtrl', function($scope, counter){
+    $scope.display;
+
+    $scope.increment = function(){
+      counter++;
+      if(counter%4 === 0){
+        $scope.display = 'FOZZ';
+      }
+      if(counter%6 === 0){
+        $scope.display = 'BAZZ';
+      }
+      if(counter%4 === 0 && counter%6 === 0){
+        $scope.display = 'FOZZBAZZ';
+      }
+    }
+  }).run();
