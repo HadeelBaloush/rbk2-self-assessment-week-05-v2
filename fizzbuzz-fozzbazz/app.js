3275d8263e91ec14b3113466fbca1d163d8a8592
HadeelBaloush <hadeelbaloush@gmail.com>
2017-02-04 11:55:43 +0400

    3

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -1,67 +1,64 @@
 angular.module('app', ['ngRoute'])
-  .config(function($routeProvider) {
-    $routeProvider
-      .when('/fizzbuzz', {
-        templateUrl: 'partials/fizzbuzz.html',
-        controller: 'fizzbuzzCtrl'
-      })
-      .when('/fozzbazz', {
-        templateUrl: 'partials/fozzbazz.html',
-        controller: 'fozzbazzCtrl'
-      })
-      .otherwise({
-        redirectTo: '/fizzbuzz'
-      });
+.config(function($routeProvider) {
+  $routeProvider
+  .when('/fizzbuzz', {
+    templateUrl: 'partials/fizzbuzz.html',
+    controller: 'fizzbuzzCtrl'
   })
+  .when('/fozzbazz', {
+    templateUrl: 'partials/fozzbazz.html',
+    controller: 'fozzbazzCtrl'
+  })
+  .otherwise({
+    redirectTo: '/fizzbuzz'
+  });
+})
 // DO NOT MODIFY CODE ABOVE THIS LINE
 
 /*  HINT: Make sure your controllers, methods, and variables 
-    are named what $routeProvider and the partials are expecting  */
+are named what $routeProvider and the partials are expecting  */
 
-  .factory('counter', function(){
-    return 0;
+.factory('counter', function(){
 
-    // {
-    //   count: 0,
-    //   // addByOne: function(){
-    //   //   count++;
-    //   // }
-    // }
-  })
-  .controller('fizzbuzzCtrl', function($scope, counter){
-    $scope.display = '';
-    $scope.increment = function(){
-      counter++;
-       $scope.display = 'FIZZ'
-       console.log(counter, 'hhhhhhh')
-      // if(counter%3 === 0){
-      //   $scope.display = 'FIZZ';
-      // }
-      // else if(counter%5 === 0){
-      //   $scope.display = 'BUZZ';
-      // }
-      // else if(counter%5 === 0 && counter%3 === 0){
-      //   $scope.display = 'FIZZBUZZ';
-      // }
-
-      // .addByOne().then(function(){
-      //   console.log('It is incremented by one');
-      // });
+  return {
+    value: 0,
+    addByOne: function(){
+      value++;
     }
-  })
-  .controller('fozzbazzCtrl', function($scope, counter){
-    $scope.display;
+  }
+})
+.controller('fizzbuzzCtrl', function($scope, counter){
+  $scope.display = '';
+  $scope.increment = function(){
+    counter.addByOne().then(function(){
+      if(counter.value%3 === 0){
+        console.log('dddddd')
+        $scope.display = 'FIZZ';
+      }
+      else if(counter.value%5 === 0){
+        $scope.display = 'BUZZ';
+      }
+      else if(counter.value%5 === 0 && counter.value%3 === 0){
+        $scope.display = 'FIZZBUZZ';
+      }
 
-    $scope.increment = function(){
-      counter++;
-      if(counter%4 === 0){
-        $scope.display = 'FOZZ';
-      }
-      if(counter%6 === 0){
-        $scope.display = 'BAZZ';
-      }
-      if(counter%4 === 0 && counter%6 === 0){
-        $scope.display = 'FOZZBAZZ';
-      }
+    })
+
+  }
+})
+.controller('fozzbazzCtrl', function($scope, counter){
+  $scope.display;
+
+  $scope.increment = function(){
+    counter.value++;
+    if(counter.value%4 === 0){
+      $scope.display = 'FOZZ';
     }
-  }).run();
+    if(counter.value%6 === 0){
+      $scope.display = 'BAZZ';
+    }
+    if(counter.value%4 === 0 && counter.value%6 === 0){
+      $scope.display = 'FOZZBAZZ';
+    }
+  }
+});
